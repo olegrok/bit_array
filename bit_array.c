0e6a0bb8389c5f73562b7a9e49705adffcfb2971
Babin Oleg <babinoleg@mail.ru>
2019-10-13 01:41:59 +0300

    Do only one allocation for bit_array

--- bit_array.c
+++ bit_array.c
@@ -10,22 +10,20 @@
 bit_array *bit_array_create(word_addr_t num_of_words)
 {
 	assert(num_of_words > 0);
-	bit_array *bitarr = (bit_array*)malloc(sizeof(bit_array));
+	size_t struct_size = sizeof(bit_array) + num_of_words * sizeof(word_t);
+
+	bit_array *bitarr = (bit_array*)calloc(1, struct_size);
 	if (bitarr == NULL) {
 		return NULL;
 	}
+
+	bitarr->words = (void*)(bitarr + 1);
 	bitarr->num_of_words = num_of_words;
-	bitarr->words = (word_t*)calloc(bitarr->num_of_words, sizeof(word_t));
-	if (bitarr->words == NULL) {
-		free(bitarr);
-		return NULL;
-	}
 	return bitarr;
 }
 
 void bit_array_free(bit_array *array)
 {
-	free(array->words);
 	free(array);
 }
 
